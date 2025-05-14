e9f3725176e76820fd2bd345da9698317dfc5d2d
Karthick Ramesh Kumar <karthickraster>
2025-05-14 23:02:50 +0530

    Creating Routing file

---
+++ server/routes/movies.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+//Create CRUD Operations for Movies
+
+//Creating C - Create
+
+app.post("/movies", () => {});
+
+//Creating R - Read
+
+app.get("/movies", () => {});
+
+//Creating U - Update
+
+app.put("/movies/:id", () => {});
+
+//Creating D - Delete
+
+app.delete("/movies/:id", () => {});
